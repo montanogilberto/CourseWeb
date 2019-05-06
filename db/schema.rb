7a3f0f3acb55f43c2c98959f69d8eae587fce717
gmontano <gilberto.montano@crol.mx>
2019-05-06 14:58:04 -0700

    add unit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190505030446) do
+ActiveRecord::Schema.define(version: 20190506214551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20190505030446) do
     t.integer "percent", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.string "name"
+    t.bigint "level_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["level_id"], name: "index_units_on_level_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 20190505030446) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "units", "levels"
 end
