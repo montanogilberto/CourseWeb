84c2767a13db345b93100ab0294a71982950768a
gmontano <gilberto.montano@crol.mx>
2019-05-10 14:14:35 -0700

    changue sideBar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,24 @@
 class DashboardController < ApplicationController
+  before_action :set_levels
+  before_action :set_units
+  before_action :set_lessons
+
   def index
   	
   end
+
+  private 
+  def set_levels
+  	@levels = Level.all
+  end
+
+  def set_units
+  	@units = Unit.joins(:level)
+  end
+
+  def set_lessons
+  	@lessons = Lesson.joins(:unit)
+  end
+
+
 end
